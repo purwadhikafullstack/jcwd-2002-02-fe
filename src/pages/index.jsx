bdd1e028a0014aae350a1cafe1973898d7b0f55a
Mychael Son <98306105+Mychaelson@users.noreply.github.com>
2022-06-06 23:02:38 +0700

    UI: fix the product card

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,7 @@ import { Button, Container, Typography } from "@mui/material";
 import { fontFamily } from "@mui/system";
 import Footer from "components/Footer";
 import MetodePembayaran from "components/MetodePembayaran";
+import ProductCard from "components/ProductCard";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
@@ -12,6 +13,7 @@ const Home = () => {
       <Container>
         <Button variant="contain">hello world</Button>
         <Typography variant="h2">hello world</Typography>
+        <ProductCard />
       </Container>
       <MetodePembayaran />
       <Footer />
