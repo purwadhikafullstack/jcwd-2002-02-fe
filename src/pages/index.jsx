3bdc33b4b723547ef2007d2542e81a3fc0a37fa7
jrmygp <fat.snorlax17@gmail.com>
2022-06-07 11:22:28 +0700

    added mui themes

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,18 +1,14 @@
-import { Box, Button, Container, Typography } from "@mui/material";
-import { fontFamily } from "@mui/system";
+import { Box } from "@mui/material";
 import Nav from "components/nav";
 import UnggahResep from "components/unggahResep";
 import Footer from "components/Footer";
 import MetodePembayaran from "components/MetodePembayaran";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
 
 const Home = () => {
   return (
     <Box>
       <Nav />
-      <UnggahResep/>
+      <UnggahResep />
       <MetodePembayaran />
       <Footer />
     </Box>
