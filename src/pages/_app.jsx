2cd0441c10b0a4a4b0261f1f51da15ef87adaaf1
Mychael Son <98306105+Mychaelson@users.noreply.github.com>
2022-06-14 23:30:27 +0700

    Fix: fix the table filtering

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -9,17 +9,11 @@ const MyApp = ({ Component, pageProps }) => {
   const router = useRouter();
   return (
     <ThemeProvider theme={theme}>
-      {router.pathname === "/admin/login" ? (
-        <Component {...pageProps} />
-      ) : (
-        <>
-          {router.pathname.startsWith("/admin") ? (
-            <AdminPageContainer children={<Component {...pageProps} />} />
-          ) : (
-            <Component {...pageProps} />
-          )}
-        </>
-      )}
+      {/* {router.pathname.startsWith("/admin") ? (
+        <AdminPageContainer children={<Component {...pageProps} />} />
+      ) : ( */}
+      <Component {...pageProps} />
+      {/* )} */}
     </ThemeProvider>
   );
 };
