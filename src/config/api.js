07b622186ccb3ee388f482442d0b5757d834c471
Mychael Son <98306105+Mychaelson@users.noreply.github.com>
2022-06-21 16:16:34 +0700

    Feat: added cookie interceptor

--- src/config/api.js
+++ src/config/api.js
@@ -1,10 +1,16 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
 // eslint-disable-next-line import/prefer-default-export
 export const API_URL = "http://localhost:2000";
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  // eslint-disable-next-line no-param-reassign
+  config.headers.authorization = Cookies.get("user_auth_token") || "";
 });
 
 export default axiosInstance;
