9b4cc27ea1c8499bf217ea747097873eb664092f
wahyuoctar <wahyuoctar@gmail.com>
2022-07-16 18:43:38 +0700

    Squashed commit of the following:
    commit 1a4684dce2661f9a2b20cdd3148ddf1323f0335c
    Author: wahyuoctar <wahyuoctar@gmail.com>
    Date:   Sat Jul 16 18:25:37 2022 +0700
    
        Update index.jsx
    
    commit 122f7f0d5f8c63cbf9c0af9ff1ccb045fc6914bd
    Author: wahyuoctar <wahyuoctar@gmail.com>
    Date:   Fri Jul 15 08:17:21 2022 +0700
    
        UI: Laba rugi page
    
    commit 587d1d9d488fcbf977ecadd0d3791608e68cb838
    Author: wahyuoctar <wahyuoctar@gmail.com>
    Date:   Thu Jun 23 13:07:54 2022 +0700
    
        [P2-5] Login Feature
    
    commit 937a39bd8399b97ccb78078486f13e7fa25f4f05
    Author: wahyuoctar <wahyuoctar@gmail.com>
    Date:   Thu Jun 23 12:46:39 2022 +0700
    
        [P2-5] Login Feature
    
    commit f4d9aff946e431e64aa8b551eae9c0f986370246
    Author: wahyuoctar <wahyuoctar@gmail.com>
    Date:   Wed Jun 22 00:03:17 2022 +0700
    
        [P2-5] Login Feature
    
    commit 01cd526012a567ccf562d9baf13fabc39df03787
    Author: wahyuoctar <wahyuoctar@gmail.com>
    Date:   Thu Jun 16 14:13:45 2022 +0700
    
        UI: RingkasanStatistik UI Added
    
    commit f88c544d6997f1a487d0884a0e55ef64d2e072e8
    Author: wahyuoctar <wahyuoctar@gmail.com>
    Date:   Wed Jun 15 12:17:15 2022 +0700
    
        Uninstall Chartjs Package
    
    commit eff179b15a68c29f962a39383ae35844df493111
    Author: wahyuoctar <wahyuoctar@gmail.com>
    Date:   Wed Jun 15 12:04:02 2022 +0700
    
        UI: dashboard UI added
    
    commit 71439aa62853aac6c1ef89708eb5858b98ff5484
    Author: wahyuoctar <wahyuoctar@gmail.com>
    Date:   Tue Jun 14 11:35:46 2022 +0700
    
        UI: Fix AdminContainer
    
    commit 859b0baeddfff2bad133c8f125df19bbd463313d
    Author: wahyuoctar <wahyuoctar@gmail.com>
    Date:   Fri Jun 10 14:56:23 2022 +0700
    
        UI: Admin Navbar added
    
    commit e81151aac1dfe742d25eee0e9e2ab80b7a41a84f
    Author: wahyuoctar <wahyuoctar@gmail.com>
    Date:   Fri Jun 10 11:25:03 2022 +0700
    
        UI: AdminSidebar added
    
    commit 38761cd08a156749fb0cd466813782d96ecbb73a
    Author: wahyuoctar <wahyuoctar@gmail.com>
    Date:   Thu Jun 9 22:11:31 2022 +0700
    
        UI: Login Page Added
    
    commit 8e50d763b3536cb454802594a2d1128c82139796
    Author: wahyuoctar <wahyuoctar@gmail.com>
    Date:   Wed Jun 8 15:23:49 2022 +0700
    
        UI: register page added

--- src/pages/admin/laba-&-rugi/index.jsx
+++ src/pages/admin/laba-&-rugi/index.jsx
@@ -17,10 +17,12 @@ import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import requiresAdmin from "config/requireAdmin";
+import moment from "moment";
+import IncomeStatement from "components/Admin/IncomeStatement";
 
 const LabaDanRugiPage = () => {
   const [periode, setPeriode] = useState("Bulanan");
-  const [bulan, setBulan] = useState("Januari");
+  const [bulan, setBulan] = useState(1);
   const [tahun, setTahun] = useState("2022");
 
   const periodeHandle = (event) => {
@@ -32,6 +34,8 @@ const LabaDanRugiPage = () => {
   const tahunHandle = (event) => {
     setTahun(event);
   };
+
+  console.log(bulan);
   return (
     <Grid container>
       {/* Heading Box */}
@@ -104,18 +108,18 @@ const LabaDanRugiPage = () => {
               displayEmpty
               autoWidth
             >
-              <MenuItem value="Januari">Januari</MenuItem>
-              <MenuItem value="Februari">Februari</MenuItem>
-              <MenuItem value="Maret">Maret</MenuItem>
-              <MenuItem value="April">April</MenuItem>
-              <MenuItem value="Mei">Mei</MenuItem>
-              <MenuItem value="Juni">Juni</MenuItem>
-              <MenuItem value="Juli">Juli</MenuItem>
-              <MenuItem value="Agustus">Agustus</MenuItem>
-              <MenuItem value="September">September</MenuItem>
-              <MenuItem value="Oktober">Oktober</MenuItem>
-              <MenuItem value="November">November</MenuItem>
-              <MenuItem value="Desember">Desember</MenuItem>
+              <MenuItem value={1}>Januari</MenuItem>
+              <MenuItem value={2}>Februari</MenuItem>
+              <MenuItem value={3}>Maret</MenuItem>
+              <MenuItem value={4}>April</MenuItem>
+              <MenuItem value={5}>Mei</MenuItem>
+              <MenuItem value={6}>Juni</MenuItem>
+              <MenuItem value={7}>Juli</MenuItem>
+              <MenuItem value={8}>Agustus</MenuItem>
+              <MenuItem value={9}>September</MenuItem>
+              <MenuItem value={10}>Oktober</MenuItem>
+              <MenuItem value={11}>November</MenuItem>
+              <MenuItem value={12}>Desember</MenuItem>
             </Select>
           </FormControl>
 
@@ -157,11 +161,76 @@ const LabaDanRugiPage = () => {
           Laporan Laba & Rugi
         </Typography>
         <Typography textAlign="center">
-          Periode Bulan Januari Tahun 2022
+          Periode Bulan {moment(bulan).format("MMMM")} Tahun{" "}
+          {moment(tahun).format("YYYY")}
         </Typography>
         <Typography textAlign="center">
           Terbit: Minggu 13 Februari, 2022 pukul 18.14 (GMT +07.00)
         </Typography>
+        <Box marginTop="20px">
+          <IncomeStatement
+            title1="Penjualan"
+            title2="dalam rupiah"
+            kategori={[
+              { kategoriName: "Penjualan Barang", value: 25000000 },
+              { kategoriName: "Total Service", value: 15000 },
+              { kategoriName: "Total Embalance", value: 0 },
+              { kategoriName: "Ongkos Kirim", value: 23000 },
+              { kategoriName: "Diskon Penjualan", value: 3000000 },
+              { kategoriName: "Retur Penjualan", value: 25000 },
+            ]}
+            footer="Penjualan Bersih"
+            footerValue="70.000.000"
+          />
+          <IncomeStatement
+            title1="Harga Pokok Penjualan"
+            kategori={[
+              { kategoriName: "Persediaan Awal", value: "68.000.000" },
+              { kategoriName: "Pembelian Kotor", value: "61.000.000" },
+              { kategoriName: "Retur Pembelian Kotor", value: 0 },
+              { kategoriName: "Mutasi Barang Masuk", value: 0 },
+              { kategoriName: "Mutasi Barang Keluar", value: 0 },
+              { kategoriName: "Persediaan Akhir", value: "70.000.000" },
+            ]}
+            footer="Harga Pokok Penjualan"
+            footerValue="59.000.000"
+          />
+          <IncomeStatement
+            title1="Pengeluaran Operasional"
+            kategori={[
+              { kategoriName: "Gaji Karyawan", value: 0 },
+              { kategoriName: "Listrik", value: 0 },
+              { kategoriName: "Air", value: 0 },
+              { kategoriName: "Telepon", value: 0 },
+              { kategoriName: "Internet", value: 0 },
+              { kategoriName: "Sewa Tempat", value: 0 },
+              { kategoriName: "Peralatan Kantor", value: 0 },
+              { kategoriName: "Biaya Pengadaan", value: 0 },
+              { kategoriName: "Biaya Operasional Lainnya", value: "96.173" },
+            ]}
+            footer="Pengeluaran Operasional"
+            footerValue="96.173"
+          />
+          <IncomeStatement
+            title1="Pendapatan Lainnya"
+            kategori={[
+              { kategoriName: "Cashback Pembelian", value: "177.939" },
+              { kategoriName: "Keuntungan Konsiyasi", value: 0 },
+            ]}
+            footer="Pendapatan Lainnya"
+            footerValue="177.939"
+          />
+          <IncomeStatement
+            title1="Laba Bersih"
+            kategori={[
+              { kategoriName: "Laba Kotor", value: "20.007.753" },
+              { kategoriName: "Pengeluaran Operasional", value: "96.173" },
+              { kategoriName: "Pendapatan Lainnya", value: "177.939" },
+            ]}
+            footer="Laba Bersih"
+            footerValue="20.089.519"
+          />
+        </Box>
       </Box>
     </Grid>
   );
